8a5a62e2cb6647362cf894f205c3d9880bb5fc4b
Guilherme Arcoverde <gui.arcoverde21@gmail.com>
2025-09-10 02:13:26 -0300

    feat: configure development database connection and add Swagger for API documentation

--- src/FinanceTracker.API/Program.cs
+++ src/FinanceTracker.API/Program.cs
@@ -1,14 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+using FinanceTracker.Infrastructure.Data;
+using FinanceTracker.Infrastructure.Migrations;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnection")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
 if (app.Environment.IsDevelopment())
 {
+    await MigrateDatabase();
 }
 
 app.UseHttpsRedirection();
@@ -18,3 +29,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+async Task MigrateDatabase()
+{
+    await using var scope = app.Services.CreateAsyncScope();
+    await DatabaseMigrations.MigrateDatabase(scope.ServiceProvider);
+}
+
+namespace FinanceTracker.API
+{
+    public partial class Program { }
+}
